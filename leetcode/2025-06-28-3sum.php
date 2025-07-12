b3f7b9953f92d8b3c4c7dd32bb4d0920a0f3a8ce
unknown <aziz.h2949@gmail.com>
2025-07-12 23:37:02 +0430

    leetcode problem (3sum) resolved using php

--- leetcode/2025-06-28-3sum.php
+++ leetcode/2025-06-28-3sum.php
@@ -1,8 +1,9 @@
+<?php 
 /**
  * LeetCode Problem: 3SUM
  * Difficulty: Easy
  * Solved: 28 June 2025
- * Language: JavaScript
+ * Language: PHP
  * Link: https://leetcode.com/problems/3sum/description/
  * Description: 
  *  Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
@@ -34,41 +35,41 @@
  * This method ensures optimal time complexity compared to brute-force solutions and is widely recognized as the 
  * standard efficient approach for the 3Sum problem.
  */
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
-var threeSum = function(nums) {
-   const result = [];
-    //1. sort the input in ascending order
-    nums.sort((a,b) => a - b);
-    //2. Iterate over the input array 
-    for(let i = 0; i < nums.length -2; i++){
-        //2.1. if current element is > 0 break the loop
-        if(nums[i] > 0) break;
-        //2.2. if current element is equal to next element skip it.
-        if(i > 0 && nums[i] === nums[i - 1]) continue;
-        let low = i + 1; high = nums.length - 1;
-        //
-        while(low < high){
-            const sum = nums[i] + nums[low] + nums[high];
-            // if sum < 0 move forward
-            if(sum < 0 ) low++
-            // if sum > 0 move backward 
-            else if(sum > 0) high--
-            else{
-                // found the triplet
-                result.push([nums[i],nums[low],nums[high]])
-                //skip duplicates
-                while(low < high && nums[low] === nums[low +1]) low++
-                while(low < high && nums[high] === nums[high  - 1]) high--
-                low++
-                high--
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer[][]
+     */
+    function threeSum($nums) {
+        $result = [];
+        sort($nums);
+        for($i = 0; $i < count($nums) - 2; $i++){
+            //handle edge cases
+            if($nums[$i] > 0) break;
+            if($i > 0 && $nums[$i] == $nums[$i - 1]) continue;
+            $low = $i + 1;
+            $high = count($nums) -1;
+
+            while($low < $high){
+                $sum = $nums[$i] + $nums[$low] + $nums[$high];
+                // if sum < 0, move forward
+                if($sum < 0 ) $low++;
+                // if sum > 0, move bakward
+                else if($sum > 0 ) $high--;
+                else {
+                    array_push($result, [$nums[$i],$nums[$low],$nums[$high]]);
+                    // Skip duplicates
+                    while($low < $high && $nums[$low] == $nums[$low + 1] ) $low++;
+                    while($low < $high && $nums[$high] == $nums[$high -1]) $high--;
+
+                    $low++;
+                    $high--;
+
+                }
             }
-
         }
-
+        return $result;
     }
-    return result;
-
- };
+}
